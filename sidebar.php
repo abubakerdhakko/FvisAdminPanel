8479feca4a965ad1e43eff33fb91ed8781f46c61
abubakar siddique <fendevdelta@jobesk.com>
2020-11-08 03:53:57 +0500

    data php

---
+++ sidebar.php
@@ -0,0 +1,34 @@
+
+      <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+          <!-- <img alt="image" src="assets/img/logo.png" class="header-logo" /> -->
+            <a href="index.html"> <span
+                class="logo-name text-white">Logo</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class=" ProfileImg  mb-3">
+                
+            <a href="#" data-toggle="dropdown"
+              class="nav-link  nav-link-lg nav-link-user nohoverUser"> <img alt="image" src="assets/img/user.png"
+                class="user-img-radious-style"> 
+                <span class="d-sm-none d-lg-inline-block ml-3">
+                <span class="bbbUN text-white">Username</span>
+                    <br/>
+            <span class="bbb">Dashboard</span>
+            </span></a>
+            </li>
+
+
+            <li class=""><a class="nav-link mt-5 bgSidebar" href="blank.html"><span>Blank Page</span></a></li>
+
+            <li class=""><a class="nav-link mt-5 bgSidebar" href="blank.html"><span>Investor</span></a></li>
+
+            <li class=""><a class="nav-link mt-5 bgSidebar" href="blank.html"><span>Payment Panel</span></a></li>
+
+            <li class=""><a class="nav-link mt-5 bgSidebar" href="blank.html"><span>Investment Panel</span></a></li>
+
+          </ul>
+        </aside>
+      </div>
